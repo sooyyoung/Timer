4060f5cafa1da5a2d225e079c58e6c7356d7c186
sooyyoung <sooyyyoung@gmail.com>
2022-06-07 11:50:44 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const play = document.querySelector('.play');
+const sec = document.querySelector('.sec .time');
+
+let count = 5;
+
+
+
+play.addEventListener('click', function(){
+    sec.textContent = count;
+    setInterval(function () {
+    count--;
+    if (count >= 0) {
+        sec.textContent = count;
+    }
+    if (count == 0) {
+        alert('Finish');
+    }
+    }, 1000);
+})
